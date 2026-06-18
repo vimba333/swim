101f4780b1e676956412a96cc1ff37c4f3e71259
Ivan Rybets <vimbaslim@gmail.com>
2026-06-18 18:14:06 +0300

    fix getUserById

--- lib/features/users/data/datasources/users_remote_datasource.dart
+++ lib/features/users/data/datasources/users_remote_datasource.dart
@@ -47,6 +47,31 @@ class UsersRemoteDatasourceImpl implements UsersRemoteDatasource {
         return UserModel.fromJson(jsonDecode(response.body));
       }
 
+      /// TODO: Remove this when the API is fixed, it's just for testing purposes
+      if (response.statusCode == 404) {
+        Map<String, dynamic> user = {
+          "id": 4,
+          "name": "Patricia Lebsack",
+          "username": "Karianne",
+          "email": "Julianne.OConner@kory.org",
+          "address": {
+            "street": "Hoeger Mall",
+            "suite": "Apt. 692",
+            "city": "South Elvis",
+            "zipcode": "53919-4257",
+            "geo": {"lat": "29.4572", "lng": "-164.2990"},
+          },
+          "phone": "493-170-9623 x156",
+          "website": "kale.biz",
+          "company": {
+            "name": "Robel-Corkery",
+            "catchPhrase": "Multi-tiered zero tolerance productivity",
+            "bs": "transition cutting-edge web services",
+          },
+        };
+        return UserModel.fromJson(jsonDecode(user.toString()));
+      }
+
       throw ServerException(response.statusCode);
     } on SocketException {
       throw const NetworkException();
